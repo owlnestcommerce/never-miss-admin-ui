a8662022ac41b092409d724618706ad378dee2da
Chhekur <820121223505e@gmail.com>
2025-10-05 18:13:00 +0530

    update

--- craco.config.js
+++ craco.config.js
@@ -1,46 +1,18 @@
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
-      // Modify CSS loaders to handle Polaris styles
-      const modifyLoaders = (loader) => {
-        if (loader.options && loader.options.plugins) {
-          loader.options.plugins = loader.options.plugins.map(plugin => {
-            // Find postcss-calc plugin
-            if (plugin && plugin[0] && plugin[0].postcssPlugin === 'postcss-calc') {
-              console.log('Found postcss-calc plugin, configuring...');
-              return [
-                plugin[0],
-                {
-                  ...plugin[1],
-                  warnWhenCannotResolve: false,
-                  preserve: true,
-                }
-              ];
+      // Disable CSS minimization to avoid postcss-calc issues
+      if (webpackConfig.optimization && webpackConfig.optimization.minimizer) {
+        webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.map(
+          minimizer => {
+            if (minimizer.constructor.name === 'CssMinimizerPlugin') {
+              // Remove CssMinimizerPlugin to avoid postcss-calc issues
+              return null;
             }
-            return plugin;
-          });
-        }
-        return loader;
-      };
-
-      // Process all CSS loaders
-      webpackConfig.module.rules.forEach(rule => {
-        if (rule.oneOf) {
-          rule.oneOf.forEach(loader => {
-            if (loader.test && loader.test.toString().includes('css')) {
-              if (loader.use) {
-                // Handle array of loaders
-                if (Array.isArray(loader.use)) {
-                  loader.use = loader.use.map(modifyLoaders);
-                } else {
-                  // Handle single loader
-                  loader.use = modifyLoaders(loader.use);
-                }
-              }
-            }
-          });
-        }
-      });
+            return minimizer;
+          }
+        ).filter(Boolean);
+      }
 
       return webpackConfig;
     },
